23ac000d12d5f3990f63d6f666ebca577d22e08b
Nandnidee <99739856+Nandnidee@users.noreply.github.com>
2024-08-04 00:50:46 +0530

    first commit

---
+++ Introduction C#/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProjects
+{
+    class stringConverter
+    {
+        public void change(string inp)
+        {
+            int a = Convert.ToInt32(inp);
+            Console.WriteLine(a.GetType());
+        }
+    }
+}
---
+++ Introduction C#/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            Console.WriteLine(a.GetType());
+            stringConverter obj = new stringConverter();
+            obj.change(a);
+
+
+        }
+    }
+}
