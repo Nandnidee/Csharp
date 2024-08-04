d6a5788df2d7c44f9f39482a5284e8c570b4dd47
Nandnidee <99739856+Nandnidee@users.noreply.github.com>
2024-08-04 10:55:06 +0530

    If Statement

---
+++ Conditional Statement/IsStatement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_Statement
+{
+    class IsStatement
+    {
+        public void isAllowedVoting(int age)
+        {
+            if(age>=18 && age <= 120 && age is int)
+            {
+                Console.WriteLine(age + " age is Allowed To vote");
+            }
+            else
+            {
+                Console.WriteLine("You are not allowed to vote");
+            }
+        }
+
+    }
+}
---
+++ Conditional Statement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = Convert.ToInt32(Console.ReadLine());
+            IsStatement obj = new IsStatement();
+            obj.isAllowedVoting(age);
+
+        }
+    }
+}
